01171a8d49861ef4910b3cf33a78442faba5e851
Alec Kinzie <aleckk18@gmail.com>
2021-10-26 12:04:39 -0500

    Initial Commit

---
+++ FinalGradeCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinalGradeCalculator
+//
+//  Created by Alec Kinzie on 10/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
